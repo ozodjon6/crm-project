07f9c5097febfc31d6718b19d0ac8e90aca6ad25
Bek <ozodjon6@gmail.com>
2021-09-11 06:22:23 -0700

    feat: add all html files and fix sidebar and btn navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
   <div id="app">
-    <router-view/>
-    <button class="btn"> btn</button>
+    <component :is="layout">
+      <router-view/>
+    </component>
   </div>
 </template>
+
+<script>
+
+import EmptyLayout from "@/layouts/EmptyLayout";
+import MainLayout from "@/layouts/MainLayout";
+
+export default {
+  computed: {
+    layout() {
+      console.log()
+      return (this.$route.meta.layout || 'empty') + '-layout'
+    }
+  },
+  components: {
+    EmptyLayout,
+    MainLayout
+  }
+}
+
+</script>
 
 <style lang="scss">
 @import "~materialize-css/dist/css/materialize.min.css";
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +7,56 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    meta: { layout: 'main' },
+    component: () => import('../views/Home')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Login')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Register')
+  },
+  {
+    path: '/detail-record',
+    name: 'DetailRecord',
+    meta: { layout: 'main' },
+    component: () => import('../views/DetalRecord')
+  },
+  {
+    path: '/history',
+    name: 'History',
+    meta: { layout: 'main' },
+    component: () => import('../views/History')
+  },
+  {
+    path: '/planning',
+    name: 'Planning',
+    meta: { layout: 'main' },
+    component: () => import('../views/Planning')
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    meta: { layout: 'main' },
+    component: () => import('../views/Profile')
+  },
+  {
+    path: '/record',
+    name: 'Record',
+    meta: { layout: 'main' },
+    component: () => import('../views/Record')
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    meta: { layout: 'main' },
+    component: () => import('../views/Categories')
   },
 ]
 
